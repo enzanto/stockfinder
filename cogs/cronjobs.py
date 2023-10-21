4ed1606b217c6c3ca6c521c037378d4e21fe581b
Fredrik Gjellestad <freddyg91@gmail.com>
2023-10-21 19:01:19 +0200

    removed full scan from cronjob in bot.

--- cogs/cronjobs.py
+++ cogs/cronjobs.py
@@ -6,72 +6,41 @@ import aiocron
 from screen import MarketScreener
 import asyncio,functools,typing
 import time
+import concurrent.futures
+import settings
+from threading import Thread
+import datetime
+logger = settings.logging.getLogger("discord")
 # intents = discord.Intents.all()
 # bot = commands.Bot(case_insensitive=True, command_prefix='!', intents=intents, help_command=None)
 
 CHANNEL_ID=1161668764341907556
-# CHANNEL_ID=1156506339019857920
+# CHANNEL_ID=1156506339019857920 #Test channel
+# warnings.simplefilter('ignore', RuntimeWarning)
+# timezone = datetime.timezone.tzname("Europe/Oslo")
+time_of_day = datetime.time(hour=23, minute=23)
 
-# def to_thread(func: typing.Callable) -> typing.Coroutine:
-#     @functools.wraps(func)
-#     async def wrapper(*args, **kwargs):
-#         return await asyncio.to_thread(func, *args, **kwargs)
-#     return wrapper
 
 class CronJobs(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-        print("Ready")
+        logger.info("Ready")
         self.scheduler = AsyncIOScheduler()
+        self.func.start()
+        self.scheduler.add_job(self.run_func, trigger="cron", day_of_week="0-4", hour="16", minute="45")
 
-        self.scheduler.add_job(self.func, trigger="cron", day_of_week="0-4", hour="17", minute="43")
-
-        #starting the scheduler
+        # starting the scheduler
         self.scheduler.start()
 
+
+
+    @tasks.loop(time=time_of_day) 
     async def func(self):
-        print(self.bot)
         channel = self.bot.get_channel(CHANNEL_ID)
         result = MarketScreener()
-        await result.rabbit.connect()
-        await result.database()
-        print("database done")
-        tasks = [result.scan(i) for i in result.stocklist.index]
-        print("all scan tasks appended, now we wait")
-        for task in asyncio.as_completed(tasks):
-            try:
-                await task
-            except Exception as e:
-                print(f"Error occured: {e}")
-        print("done awaiting tasks")
-        embeds = []
-        embeds2 = []
-        images = []
-        images2 = []
-        finished_result = sorted(result.result['result'], key=lambda x: x['stock'])
-        for i in finished_result:
-            if i['trend'] >= 7 and "image investtech" in i:
-                embeds2.extend(i['embed'])
-                images2.extend(i['image'])
-            elif i['trend'] >= 7 and "image investtech" not in i:
-                embeds.append(i['embed'])
-                images.append(i['image'])
-        print(embeds)
-        length=6
-        for i in range(0, len(embeds2), length):
-            x=i
-            print(i,x)
-            emb = embeds2[x:x+length]
-            im = images2[x:x+length]
-            await channel.send(embeds=emb, files=im, silent=True, delete_after=20)
-        if len(embeds) > 0:
-            for i in range(0, len(embeds), length):
-                x=i
-                print(i,x)
-                emb = embeds[x:x+length]
-                im = images[x:x+length]
-                await channel.send(embeds=emb, files=im, silent=True, delete_after=20)
-        print("done sending")
+        await result.rabbit.connect() #awaited
+        await result.database(engine=settings.engine)#awaited
+        logger.info("database done")
         await result.rabbit.disconnect()
 
 async def setup(bot):
