4b54588bfcfb928931c0d60b3650d1faa1f788b7
Fredrik Gjellestad <freddyg91@gmail.com>
2023-11-02 20:46:03 +0100

    added a build script because i am lazy

---
+++ buildx.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "enter version number for stockfinder docker image"
+echo "last version = v1.4.2"
+read VERSION
+script_path="$0"
+full_path="$(cd "$(dirname "$script_path")" && pwd)/$(basename "$script_path")"
+echo $full_path
+echo "you have selected $VERSION"
+sed -i "0,/=/{s/=.*/= $VERSION\"/}" $full_path
+docker buildx build --platform linux/amd64,linux/arm64 -t enzanto/stockfinder -t enzanto/stockfinder:$VERSION --push .
