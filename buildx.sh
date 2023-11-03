bcd832ef551a8af10c9c4d033df20be04257a9f3
Fredrik Gjellestad <freddyg91@gmail.com>
2023-11-03 22:07:49 +0100

    buildt new image

--- buildx.sh
+++ buildx.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo "enter version number for stockfinder docker image"
-echo "last version = v1.4.4"
+echo "last version = v1.4.5"
 read VERSION
 script_path="$0"
 full_path="$(cd "$(dirname "$script_path")" && pwd)/$(basename "$script_path")"
