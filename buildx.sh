a72cc4dec779e7e0aa626db47484ea3ac794b1e5
Fredrik Gjellestad <freddyg91@gmail.com>
2024-06-22 17:25:40 +0200

    improved build script, set numpy version as pandas-ta is not compatible with numpy 2.0 yet

--- buildx.sh
+++ buildx.sh
@@ -1,10 +1,16 @@
 #! /bin/bash
 echo "enter version number for stockfinder docker image"
-echo "last version = v1.5.8"
+echo "last version = v1.5.9"
 read VERSION
 script_path="$0"
 full_path="$(cd "$(dirname "$script_path")" && pwd)/$(basename "$script_path")"
 echo $full_path
 echo "you have selected $VERSION"
-sed -i "0,/=/{s/=.*/= $VERSION\"/}" $full_path
-docker buildx build --platform linux/amd64,linux/arm64 -t enzanto/stockfinder -t enzanto/stockfinder:$VERSION --push .
+if [ "$VERSION" = "development" ]; then
+    echo "tagging build with $VERSION"
+    docker buildx build --platform linux/amd64,linux/arm64  -t enzanto/stockfinder:$VERSION --push .
+else
+    echo "tagging build with latest,$VERSION"
+    sed -i "0,/=/{s/=.*/= $VERSION\"/}" $full_path
+    # docker buildx build --platform linux/amd64,linux/arm64 -t enzanto/stockfinder -t enzanto/stockfinder:$VERSION --push .
+fi
--- dockerfile
+++ dockerfile
@@ -3,9 +3,10 @@ FROM python:3.10-slim
 
 WORKDIR /usr/src/app
 ENV TZ=Europe/Oslo
+ENV LOGLEVEL=INFO
 #RUN python -m pip install --upgrade pip
 COPY requirements.txt ./
-run apt update && apt install pip --upgrade -y
+RUN apt update && apt install pip --upgrade -y
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 #RUN cp crontab /etc/cron.d/stockfinder
